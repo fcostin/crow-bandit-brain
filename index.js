a053f427898ee8a5e6c02b456934bff9c82917db
rfc <reuben.fletchercostin@gmail.com>
2020-09-27 09:41:38 +1000

    define inputs for sim parameters

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+const default_param_input_defns = [
+    "stimulus-red",
+    "stimulus-blue",
+    "no-stimulus-red",
+    "no-stimulus-blue",
+]
+
+const default_param_action_defns = [
+    "yes",
+    "no",
+]
+
+const default_param_input_action_reward_defns = [
+    "stimulus-red yes 1",
+    "stimulus-red no 0",
+    "stimulus-blue yes 1",
+    "stimulus-blue no 0",
+    "no-stimulus-red yes 0",
+    "no-stimulus-red no 1",
+    "no-stimulus-blue yes 0",
+    "no-stimulus-blue no 1",
+]
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        param_input_defns: default_param_input_defns.join("\n"),
+        param_action_defns: default_param_action_defns.join("\n"),
+        param_input_action_reward_defns: default_param_input_action_reward_defns.join("\n"),
     }
 });
