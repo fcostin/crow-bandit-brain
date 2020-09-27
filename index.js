7db67c3fedfc60adc1004d8449e2bdcf57b4fb10
rfc <reuben.fletchercostin@gmail.com>
2020-09-27 20:05:26 +1000

    better ui + single step

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const default_param_input_defns = [
 ]
 
 const default_param_action_defns = [
-    "yes",
-    "no",
+    "react",
+    "wait",
 ]
 
 const default_param_input_action_reward_defns = [
-    "stimulus-red yes 1",
-    "stimulus-red no 0",
-    "stimulus-blue yes 0",
-    "stimulus-blue no 1",
-    "no-stimulus-red yes 0",
-    "no-stimulus-red no 1",
-    "no-stimulus-blue yes 1",
-    "no-stimulus-blue no 0",
+    "stimulus-red react 1",
+    "stimulus-red wait 0",
+    "stimulus-blue react 0",
+    "stimulus-blue wait 1",
+    "no-stimulus-red react 0",
+    "no-stimulus-red wait 1",
+    "no-stimulus-blue react 1",
+    "no-stimulus-blue wait 0",
 ]
 
 var app = new Vue({
@@ -29,8 +29,9 @@ var app = new Vue({
         param_input_action_reward_defns: default_param_input_action_reward_defns.join("\n"),
         ui_mode: "edit",
         out_debug: "",
-        out_columns: ["Input", "Action", "Value", "UCB"],
+        out_columns: [],
         out_rows: [],
+        sim: null,
     },
     computed: {
         is_edit_disabled: function() {
@@ -40,47 +41,45 @@ var app = new Vue({
             return this.ui_mode != "run"
         },
     },
+    watch: {
+        ui_mode: function(mode) {
+            // reset sim to avoid running with stale data that
+            // don't include latest edits.
+            if (mode === "edit") {
+                this.sim = null;
+                this.out_columns = [];
+                this.out_rows = [];
+                this.out_debug = [];
+            }
+        }
+    },
     methods: {
         step: function(event) {
-            const result = step({
-                "param_input_defns": this.param_input_defns,
-                "param_action_defns": this.param_action_defns,
-                "input_action_rewards": this.param_input_action_reward_defns,
-            })
-            this.out_debug = result.event_log;
-            this.out_columns = result.out_columns;
-            this.out_rows = result.out_rows;
+            if (this.sim == null) {
+                const ctx = {
+                    "inputs":  parseLines(parseName, this.param_input_defns),
+                    "actions": parseLines(parseName, this.param_action_defns),
+                    "input_action_rewards": parseLines(parseInputActionReward, this.param_input_action_reward_defns),
+                };
+                this.sim = makeSimulation(ctx);
+            }
+
+            let event_log = [];
+
+            function emit(args) {
+                event_log.push(args.join("  "));
+            }
+
+            const snapshot = this.sim.step(emit);
+
+            const snapshotTable = makeSnapshotTable(snapshot);
+
+            this.out_debug = event_log;
+            this.out_columns = snapshotTable.out_columns;
+            this.out_rows = snapshotTable.out_rows;
         }
     }
 });
-
-function step(params) {
-    const inputs = parseLines(parseName, params.param_input_defns);
-    const actions = parseLines(parseName, params.param_action_defns);
-    const input_action_rewards = parseLines(parseInputActionReward, params.input_action_rewards);
-
-    const ctx = {
-        "inputs": inputs,
-        "actions": actions,
-        "input_action_rewards": input_action_rewards,
-    }
-
-    let event_log = [];
-
-    function emit(args) {
-        event_log.push(args.join("  "));
-    }
-
-    const n_steps = 20;
-
-    const result = simulate(n_steps, ctx, emit);
-
-    return {
-        "event_log": event_log,
-        "out_columns": result.out_columns,
-        "out_rows": result.out_rows,
-    }
-}
 
 function* imap(f, xs) {
     for (const x of xs) {
@@ -132,6 +131,167 @@ function argmax(kvs) {
     return randomChoice(kstars);
 }
 
+// define some constants
+const BIGV = 10.0; // BIGV is a finite but very enticing value.
+const C = 1.0; // C is explore-exploit tradeoff parameter.
+
+// Uncertainty bound used when estimating the value of doing
+// action a in response to input i. . n_ is the number of
+// times we've encountered the input i before. z_ is the number
+// of times we've tried doing action a in response to i.  
+const uncertainty = function(n_, z_) {
+    return (z_ === 0.0) ? BIGV : Math.sqrt(Math.log(n_) / (1.0 * z_));
+};
+
+function makeSimulation(ctx) {
+    const sim = new Object();
+    // notation: define shorthand sets of indices
+    sim.A = new Set(ctx.actions);
+    sim.I = new Set(ctx.inputs);
+    sim.IA = new Set();
+    for (const i of sim.I) {
+        for (const a of sim.A) {
+            sim.IA.add(_ia(i, a));
+        }
+    }
+    // Define mapping from (input, action) to reward
+    sim.rewards = new Map(imap(
+        x => [_ia(x.input, x.action), x.reward],
+        ctx.input_action_rewards
+    ));
+
+    // initialise bandit memory
+
+    // n is how many times we've seen input i.
+    sim.n = new Map(imap(i => [i, 0.0], sim.I));
+
+    // Z is how many times we've done action a in response to input i.
+    sim.z = new Map(imap(ia => [ia, 0.0], sim.IA));
+
+    // V is our prior for the value of doing action a in repsonse to input i.
+    sim.v = new Map(imap(ia => [ia, 0.5], sim.IA));
+
+    sim.step = function(emit) {
+        // The environment generates an input i' for us to observe.
+        const i_prime = randomChoice(ctx.inputs);
+        emit(['crow-bandit observed input:', i_prime]);
+
+        // Observe the input i' with our sensors
+        const s = new Map(imap(i => [i, 1.0*(i === i_prime)], sim.I));
+
+        // emit(['Sensed', fmtMap(s)]);
+
+        // Decide which action a* to do
+        // emit(['Considered action values', fmtMap(sim.v)]);
+        const ucb = new Map(imap(
+            ia => [ia, sim.v.get(ia) + C * uncertainty(sim.n.get(_ia_i(ia)), sim.z.get(ia))],
+            sim.IA
+        ));
+        // emit(['Considered UCB', fmtMap(ucb)]);
+        const p = new Map(imap(
+            a => [a, sum(imap(i => ucb.get(_ia(i, a)) * s.get(i), sim.I))],
+            sim.A
+        ));
+        // emit(['Considered action-potentials', fmtMap(p)]);
+        const a_star = argmax(p);
+        emit(['crow-bandit performed action:', a_star]);
+
+        // Respond to i' by doing a* and see what reward r the environment gives us
+        const r = sim.rewards.get(_ia(i_prime, a_star));
+        if (r === 1.0) {
+            emit(['crow-bandit demonstrated the desired action']);
+            emit(['crow-bandit got a reward']);
+        } else {
+            emit(['crow-bandit failed to demonstrate the desired action']);
+            emit(['crow-bandit did not get a reward']);
+        }
+        
+
+        // Update how many times we've observed i' and done a* in response to i'
+        const succ_n = new Map(imap(i => [i, sim.n.get(i) + 1.0*(i === i_prime)], sim.I));
+
+        const succ_z = new Map(imap(
+            ia => [ia, sim.z.get(ia) + 1.0 * (_ia_i(ia) === i_prime && _ia_a(ia) === a_star)],
+            sim.IA
+        ));
+
+        // Update our belief of how valuable doing a* in response to i' is.
+        const succ_v = new Map(imap(
+            ia => [ia, (
+                (_ia_i(ia) === i_prime && _ia_a(ia) === a_star) ?
+                (sim.v.get(ia) * sim.z.get(ia) + r) / (sim.z.get(ia) + 1.0):
+                sim.v.get(ia)
+            )],
+            sim.IA
+        ));
+
+        // Advance
+        sim.n = succ_n;
+        sim.z = succ_z;
+        sim.v = succ_v;
+
+        const snapshot = {
+            'n': sim.n,
+            'z': sim.z,
+            'v': sim.v,
+            'ucb': ucb,
+            'I': sim.I,
+            'A': sim.A,
+            'IA': sim.IA,
+        };
+        return snapshot
+    }
+
+    return sim
+}
+
+function makeSnapshotTable(snapshot) {
+    const out_columns = ["input", "action", "n", "z", "value", "UCB"];
+    let out_rows = [];
+    for (const ia of snapshot.IA) {
+        let row = {
+            "input":_ia_i(ia),
+            "action":_ia_a(ia),
+            "n": snapshot.n.get(_ia_i(ia)),
+            "z": snapshot.z.get(ia),
+            "value":snapshot.v.get(ia),
+            "UCB":Number(snapshot.ucb.get(ia)).toFixed(3),
+        };
+        out_rows.push(row);
+    }
+
+    return {
+        "out_columns": out_columns,
+        "out_rows": out_rows,
+    }
+}
+
+function parseLines(parseLine, text) {
+    const lines = text.match(/[^\r\n]+/g);
+    return lines.map(parseLine);
+}
+
+function parseName(line) {
+    let names = line.match(/[A-Za-z][A-Za-z0-9-_]*/g);
+    return names[0];
+}
+
+function parseInputActionReward(line) {
+    let m = line.match(/(?<input>[A-Za-z][A-Za-z0-9-_]*)[\s]+(?<action>[A-Za-z][A-Za-z0-9-_]*)[\s]+(?<reward>[+-]?\d+(\.\d+)?)/);
+    return {
+        "input": m.groups.input,
+        "action": m.groups.action,
+        "reward": parseFloat(m.groups.reward),
+    }
+}
+
+function randomChoice(items) {
+    return items[Math.floor(items.length * Math.random())];
+}
+
+
+
+/*
 function simulate(n_steps, ctx, emit) {
     // notation: define shorthand sets of indices
     const A = new Set(ctx.actions);
@@ -150,28 +310,7 @@ function simulate(n_steps, ctx, emit) {
         ctx.input_action_rewards
     ));
 
-    // define some constants
-    const BIGV = 10.0; // BIGV is a finite but very enticing value.
-    const C = 1.0; // C is explore-exploit tradeoff parameter.
-
-    // Uncertainty bound used when estimating the value of doing
-    // action a in response to input i. . n_ is the number of
-    // times we've encountered the input i before. z_ is the number
-    // of times we've tried doing action a in response to i.  
-    const uncertainty = function(n_, z_) {
-        return (z_ === 0.0) ? BIGV : Math.sqrt(Math.log(n_) / (1.0 * z_));
-    };
-
-    // initialise bandit memory
-
-    // n is how many times we've seen input i.
-    let n = new Map(imap(i => [i, 0.0], I));
-
-    // Z is how many times we've done action a in response to input i.
-    let z = new Map(imap(ia => [ia, 0.0], IA));
-
-    // V is our prior for the value of doing action a in repsonse to input i.
-    let v = new Map(imap(ia => [ia, 0.5], IA));
+    
 
 
     let ucb = new Map();
@@ -246,30 +385,7 @@ function simulate(n_steps, ctx, emit) {
     return {
         "out_columns": out_columns,
         "out_rows": out_rows,
-    }
-    
-        
+    }        
 }
 
-function parseLines(parseLine, text) {
-    const lines = text.match(/[^\r\n]+/g);
-    return lines.map(parseLine);
-}
-
-function parseName(line) {
-    let names = line.match(/[A-Za-z][A-Za-z0-9-_]*/g);
-    return names[0];
-}
-
-function parseInputActionReward(line) {
-    let m = line.match(/(?<input>[A-Za-z][A-Za-z0-9-_]*)[\s]+(?<action>[A-Za-z][A-Za-z0-9-_]*)[\s]+(?<reward>[+-]?\d+(\.\d+)?)/);
-    return {
-        "input": m.groups.input,
-        "action": m.groups.action,
-        "reward": parseFloat(m.groups.reward),
-    }
-}
-
-function randomChoice(items) {
-    return items[Math.floor(items.length * Math.random())];
-}
+*/
