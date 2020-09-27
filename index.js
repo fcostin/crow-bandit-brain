823527801fe8413a2472ea4f02661bd366df4462
rfc <reuben.fletchercostin@gmail.com>
2020-09-27 10:43:39 +1000

    add radio buttons for edit <-> run mode

--- index.js
+++ index.js
@@ -27,5 +27,11 @@ var app = new Vue({
         param_input_defns: default_param_input_defns.join("\n"),
         param_action_defns: default_param_action_defns.join("\n"),
         param_input_action_reward_defns: default_param_input_action_reward_defns.join("\n"),
+        ui_mode: "edit",
+    },
+    computed: {
+        is_editing_disabled: function() {
+            return this.ui_mode != "edit"
+        },
     }
 });
